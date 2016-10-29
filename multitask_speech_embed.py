4684c56913a8b8ef31b5fb833701e81ee62aa8d5
Qinlan Shen <qinlanshen@gmail.com>
2016-10-29 18:14:41 -0400

    Added stuff to network skeleton

--- multitask_speech_embed.py
+++ multitask_speech_embed.py
@@ -1,7 +1,10 @@
 import constants
+import numpy as np
+import theano
+import theano.tensor as T
 
 class Instance:
-    __init__(self, filename, multitask_flag):
+    def __init__(self, filename, multitask_flag):
         self.input_file = filename  # Name of file containing data for the instance
         self.vec = []               # Vector representation for network input
         self.multitask_flag = 0     # Flag for whether to train on multiple tasks 
@@ -9,3 +12,11 @@ class Instance:
 
     def read_vec():
         # TODO: Read self.input_file, assign values to vec and task_labels
+
+class MultitaskNetwork:
+    def __init__(self, config):
+        self.config = config
+        self.batch_size = config["batch_size"]
+        self.learning_rate = config["learning_rate"]
+
+        
