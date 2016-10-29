3108296925c7903a0fee2b1fe7aef254b1052272
Dan Clothiaux <dclothia@andrew.cmu.edu>
2016-10-29 18:13:18 -0400

    added read in caller info nonsense

--- multitask_speech_embed.py
+++ multitask_speech_embed.py
@@ -1,4 +1,37 @@
 import constants
+import sys
+
+
+class CallerInfo:
+	__init__(self, userid, gender = 0, age = 0, education = 0, dialect = 0):
+		self.userid = userid
+		self.gender = gender #0 female 1 male
+		self.age = age
+		self.education = education
+		self.dialect = dialect
+
+caller_info_dic = dict() #a dictionary of conversation ids to callers [a,b]
+
+def load_caller_info():
+	callers = [s.strip().split(',') for s in open('conv_tab.csv')]
+	education_dict = {}
+
+	for caller in callers:
+		call_id = int(caller[0])
+		gender = caller[3]
+		if gender == "Female":
+			gender = 0
+		else:
+			gender = 1
+		age = 1997 - int(callers[4])
+		dialect = callers[5]
+		education = callers[6]
+		if education in education_dict:
+			education = education_dict[education]
+		else:
+			education_dict[education] = len(education_dict)
+		caller_info_dic[call_id] = CallerInfo(userid, gender, age, education, dialect)
+
 
 class Instance:
     __init__(self, filename, multitask_flag):
