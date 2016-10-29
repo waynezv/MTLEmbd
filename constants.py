5ac304f3a12234fdaa86a93466230f9045685d49
Qinlan Shen <qinlanshen@gmail.com>
2016-10-29 17:47:26 -0400

    Filled in more of skeleton

---
+++ constants.py
@@ -0,0 +1,1 @@
+TASKS = ["word", "speaker_id", "gender", "age", "education", "dialect"]
--- multitask_speech_embed.py
+++ multitask_speech_embed.py
@@ -1,5 +1,11 @@
+import constants
 
 class Instance:
-    __init__(self):
-        self.multitask_flag = 0 # Flag for whether to train 
-        self.
+    __init__(self, filename, multitask_flag):
+        self.input_file = filename  # Name of file containing data for the instance
+        self.vec = []               # Vector representation for network input
+        self.multitask_flag = 0     # Flag for whether to train on multiple tasks 
+        self.task_labels = dict()   # Labels for each task for the instance
+
+    def read_vec():
+        # TODO: Read self.input_file, assign values to vec and task_labels
