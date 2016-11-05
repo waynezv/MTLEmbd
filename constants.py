d49db13dd74ad47813cc026f3983912d57ce21f1
Dan Clothiaux <dclothia@andrew.cmu.edu>
2016-11-05 17:53:59 -0400

    updated constants

--- constants.py
+++ constants.py
@@ -1,1 +1,22 @@
+import numpy as np
+
 TASKS = ["word", "speaker_id", "gender", "age", "education", "dialect"]
+FRAMES_PER_WORD = 200
+
+CONV1_PARAM_SIZE = (1, 1, 8, 8)
+
+CONV1_PARAM_BOUND = np.sqrt(1*1*64)
+CONV1_BIAS_SIZE = (8,)
+
+MAXPOOL_SHAPE = (4,4)
+MAXPOOL_STRIDE = (2,2)
+
+MEAN_KERNEL = (3,3)
+
+CONV2_PARAM_SIZE = (1, 1, 8, 8)
+
+CONV2_PARAM_BOUND = np.sqrt(1*1*64)
+CONV2_BIAS_SIZE = (8,)
+
+FORWARD1_PARAM_SIZE = (1024, 1024)
+FORWARD1_BIAS_SIZE = (1024)
