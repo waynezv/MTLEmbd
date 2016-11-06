96c9733862a50a2ef315b6c45f2b23c1434d244f
wzhao1 <wzhao1@andrew.cmu.edu>
2016-11-06 01:33:55 -0400

    Feat(AlignWordFbank): Extract fbanks from raw_fbank.*.txt
    TODO:
    1. Split frames.
    2. Add info fields.

--- align_word_fbank.py
+++ align_word_fbank.py
@@ -1,10 +1,12 @@
 from __future__ import print_function
 import os
 import csv
+import re
 import argparse as agp
 import numpy as np
 
-FBANK_TXT_DIR = '/home/caiwch/eesen/asr_egs/swbd/v1/fbank/fbank_txt'
+# FBANK_TXT_DIR = '/home/caiwch/eesen/asr_egs/swbd/v1/fbank/fbank_txt'
+FBANK_TXT_DIR = '.'
 
 class AlignWordFbank(object):
     def __init__(self):
@@ -12,28 +14,34 @@ class AlignWordFbank(object):
 
     def split_fbank(self, FBANK_TXT_DIR):
         with open(os.path.join(FBANK_TXT_DIR, 'raw_fbank_train.1.txt'), 'r') as f:
-            line = f.readline()
-            fbank_id = line.split(' ')[0]
-            utt_id = line.split('_')[0]
-            chn_id = utt_id.split('-')[1]
-            tm_frame = [line.split('_')[1].split('-')[0], \
-                        line.split('_')[1].split('-')[1]]
-            self.word['word'] = 'test'
-            self.word['fbank_id'] = fbank_id
-            self.word['utter_id'] = utt_id
-            self.word['channel'] = chn_id
-            self.word['time_frame'] = tm_frame
-            self.word['position'] = 1
-            print(self.word)
-            tmp = []
-            for nxt_line in f:
-                tmp.append(tmp)
-                tmp = np.array(tmp)
-            print(tmp)
-            print(tmp.shape)
-            #next_line = f.readline()
-            #next_line = np.array(next_line)
-            #print(next_line.shape)
+            pat1 = re.compile('.+\[\n')
+            pat2 = re.compile('.+\]\n')
+            fbank = []
+            for line in f:
+                if pat1.match(line):
+                    print(line.split(' ')[0])
+                    fbank_id = line.split('_')[0]
+                    utt_id = fbank_id.split('-')[0]
+                    chn_id = fbank_id.split('-')[1]
+                    self.word['utter_id'] = utt_id
+                    self.word['channel'] = chn_id
+                    self.word['speaker_id'] = '00001'
+                    self.word['word'] = 'test'
+                    self.word['position'] = 1
+                    self.word['fbank'] = []
+                else:
+                    if line.split()[-1] == ']':
+                        tmp = line.split()[:-1]
+                        tmp = np.array(tmp).astype(float)
+                        fbank.append(tmp)
+                    else:
+                        tmp = line.split()
+                        tmp = np.array(tmp).astype(float)
+                        fbank.append(tmp)
+            self.word['fbank'] = np.array(fbank).reshape(-1,40)
+            print(self.word['fbank'])
+            print(self.word['fbank'].shape)
+
 
     def write_to_file(self):
         save_path = os.path.join(self.word['utter_id'], \
