9289bbf44d2730c004d6723106bdce1060a9cd47
Qinlan Shen <qinlanshen@gmail.com>
2016-11-19 15:25:33 -0500

    Updated constants

--- constants.py
+++ constants.py
@@ -4,6 +4,7 @@ TASKS = ["word", "speaker_id", "gender", "age", "education", "dialect"]
 FRAMES_PER_WORD = 200
 FRAME_SIZE = 40
 
+# Network architecture parameters
 CONV1_FILTER_SIZE = (1,1,8,8)
 CONV1_BIAS_SIZE = (1,)
 CONV1_STRIDE = (8,1)
@@ -18,3 +19,8 @@ CONV2_BIAS_SIZE = (8,)
 
 FORWARD1_FILTER_SIZE = (1024, 1024)
 FORWARD1_BIAS_SIZE = (1024,)
+
+# Training parameters
+BATCH_SIZE = 20
+IMPROVEMENT_THRESHOLD = 0.995
+VALIDATION_FREQUENCY = 10
