1a4f95dd055d9230620e40afa7b09b8ee040dafe
caiwch <caiwch@panda-jri.(none)>
2016-11-23 07:08:57 +0800

    Feat(multitask.py): Train on one minibatch, single task
    BUG:
    1. Forward layer dimension not match.

--- align_word_fbank.py
+++ align_word_fbank.py
@@ -7,7 +7,7 @@ import numpy as np
 
 # FBANK_TXT_DIR = '/home/caiwch/eesen/asr_egs/swbd/v1/fbank/fbank_txt'
 FBANK_TXT_DIR = '.'
-SAVE_DIR = './tmp'
+SAVE_DIR = './tmp2_11_19'
 
 T_WINDOW = 0.02 # sec, NOTE: not sure
 T_WINDOW_OVERLAP = 0.01 # sec
@@ -20,6 +20,7 @@ class AlignWordFbank(object):
         self.trans_info = []
         self.fnum_frame = [] # list of num. frames in each utterance for one fbank file
         self.anum_word = [] # list of num. words in each utterance for all fbank files
+        self.word_dict = {}
 
     def load_conv_info(self):
         conv_info = [s.strip().split(',') for s in open('conv_tab.csv')]
@@ -30,6 +31,9 @@ class AlignWordFbank(object):
             self.conv_info[conv_id] = {'A': call_id, 'B': recv_id}
 
     def load_word_trans(self):
+        """
+        Load word info. and build word dictionry.
+        """
         word_trans = [s for s in open('all_words_trans.txt')]
         pos = 0 # word position in utterance
         utt_id = 2 # begining utterance index
@@ -39,6 +43,8 @@ class AlignWordFbank(object):
             if parts[3] == '[silence]': # skip silence
                 continue
             word = parts[3]
+            if word not in self.word_dict:
+                self.word_dict[word] = len(self.word_dict)
             start_time = float(parts[1])
             end_time = float(parts[2])
             p1 = parts[0].split('-')
@@ -55,13 +61,19 @@ class AlignWordFbank(object):
                                     'utt_id': utt_id, 'conv_id': conv_id, \
                                     'start_time': start_time, 'end_time': end_time, \
                                     'position': pos})
+# Save word dictionary
+        out = ""
+        with open('word_dict.txt', 'w') as f:
+            for word in self.word_dict:
+                out += word + ',' + str(self.word_dict[word]) + '\n'
+            f.write(out)
 
     def _split_list(self, l, n):
         # Split a list l to almostly even n parts
         return np.array_split(l,n)
 
     def pair_word_fbank(self):
-        fbank_flist = [s.strip() for s in open('raw_fbank_train.list')]
+        fbank_flist = [s.strip() for s in open('raw_fbank_train_1.list')]
         word_ind = 0 # index for word in self.trans_info
         utt_ind = 0 # index for utt in self.anum_word
         for fbank_file in fbank_flist:
@@ -111,6 +123,8 @@ class AlignWordFbank(object):
                         tmp = np.array(tmp).astype(float)
                         fbank.append(tmp)
                         frm_cnt += 1
+                        print('num_words', num_words, ' wrd_cnt ', wrd_cnt, \
+										'seglistlastind', seglist[wrd_cnt][-1])
                         if frm_cnt == seglist[wrd_cnt][-1]: # last ind for this word
                             self.word['fbank'] = np.array(fbank).reshape(-1,40)
                             self.save_word_fbank()
@@ -220,4 +234,4 @@ if __name__ == '__main__':
     word.load_conv_info()
     word.load_word_trans()
     # word.split_fbank()
-    word.pair_word_fbank()
+#    word.pair_word_fbank()
